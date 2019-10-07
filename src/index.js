72cb8025d6be6b59ce1bb22351e2ee6e471e438e
Your Name <osekovs@mail.ru>
2019-10-07 19:38:42 +0300

    done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
+      if(knowsProgramming){
+        return Math.ceil(800/config[focus])
+      }
+      else 
+      return Math.ceil(800*1.625/config[focus])
       return 0;
   };
   
